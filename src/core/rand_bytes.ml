942a56c87979d27cafe5a495e46fffac55a8688c
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-12-17 15:51:07 -0500

    use mutex again to protect rand_bytes state
    shard over 8 distinct random generators, though.

--- src/core/rand_bytes.ml
+++ src/core/rand_bytes.ml
@@ -1,38 +1,47 @@
-let initialized_ = Atomic.make false
+open struct
+  let rand = Array.init 8 (fun _ -> Random.State.make_self_init ())
 
-let[@inline never] actually_init () = Random.self_init ()
+  let mutex = Array.init 8 (fun _ -> Mutex.create ())
 
-let[@inline] maybe_init () =
-  if not (Atomic.exchange initialized_ true) then actually_init ()
+  let ( let@ ) = ( @@ )
+end
+
+(** What rand state do we use? *)
+let[@inline] shard () : int = Thread.id (Thread.self ()) land 0b111
 
 let default_rand_bytes_8 () : bytes =
-  maybe_init ();
+  let shard = shard () in
+  let@ () = Util_mutex.protect mutex.(shard) in
+  let rand = rand.(shard) in
+
   let b = Bytes.create 8 in
   for i = 0 to 1 do
     (* rely on the stdlib's [Random] being thread-or-domain safe *)
-    let r = Random.bits () in
+    let r = Random.State.bits rand in
     (* 30 bits, of which we use 24 *)
     Bytes.set b (i * 3) (Char.chr (r land 0xff));
     Bytes.set b ((i * 3) + 1) (Char.chr ((r lsr 8) land 0xff));
     Bytes.set b ((i * 3) + 2) (Char.chr ((r lsr 16) land 0xff))
   done;
-  let r = Random.bits () in
+  let r = Random.State.bits rand in
   Bytes.set b 6 (Char.chr (r land 0xff));
   Bytes.set b 7 (Char.chr ((r lsr 8) land 0xff));
   b
 
 let default_rand_bytes_16 () : bytes =
-  maybe_init ();
+  let shard = shard () in
+  let@ () = Util_mutex.protect mutex.(shard) in
+  let rand = rand.(shard) in
+
   let b = Bytes.create 16 in
   for i = 0 to 4 do
-    (* rely on the stdlib's [Random] being thread-or-domain safe *)
-    let r = Random.bits () in
+    let r = Random.State.bits rand in
     (* 30 bits, of which we use 24 *)
     Bytes.set b (i * 3) (Char.chr (r land 0xff));
     Bytes.set b ((i * 3) + 1) (Char.chr ((r lsr 8) land 0xff));
     Bytes.set b ((i * 3) + 2) (Char.chr ((r lsr 16) land 0xff))
   done;
-  let r = Random.bits () in
+  let r = Random.State.bits rand in
   Bytes.set b 15 (Char.chr (r land 0xff));
   (* last byte *)
   b
