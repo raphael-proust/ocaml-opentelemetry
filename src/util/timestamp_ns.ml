81ec17b282a00ec4de11ba97edd9ebbb0aa75c81
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-12-17 11:54:30 -0500

    merge back ptime clock into `Clock`, make it default
    we already depend on ptime in the same package and it's not a big cost.

--- src/util/timestamp_ns.ml
+++ src/util/timestamp_ns.ml
@@ -5,4 +5,17 @@
 
 type t = int64
 
-let pp_debug out (self : t) = Format.fprintf out "<timestamp: %Ld ns>" self
+open struct
+  let ns_in_a_day = Int64.(mul 1_000_000_000L (of_int (24 * 3600)))
+end
+
+let pp_debug out (self : t) =
+  let d = Int64.(to_int (div self ns_in_a_day)) in
+  let ns = Int64.(rem self ns_in_a_day) in
+  let ps = Int64.(mul ns 1_000L) in
+  match Ptime.Span.of_d_ps (d, ps) with
+  | None -> Format.fprintf out "ts: <%Ld ns>" self
+  | Some span ->
+    (match Ptime.add_span Ptime.epoch span with
+    | None -> Format.fprintf out "ts: <%Ld ns>" self
+    | Some ptime -> Ptime.pp_human () out ptime)
