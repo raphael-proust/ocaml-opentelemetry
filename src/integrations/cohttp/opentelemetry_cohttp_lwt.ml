dc21341d92d566eabb504e904e3a424d44bd3121
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-12-27 20:17:33 -0500

    try to fix compat with versions of cohttp
    it seems like now IO is a required component of Cohttp_lwt.S.Client, so
    now we should include parameter `C` so that it just contains whatever
    the client has, minus the functions we change.

--- src/integrations/cohttp/opentelemetry_cohttp_lwt.ml
+++ src/integrations/cohttp/opentelemetry_cohttp_lwt.ml
@@ -149,21 +149,7 @@ let client ?(tracer = Otel.Tracer.dynamic_main) ?(span : Otel.Span.t option)
 
     (*   These types and values are not customized by our client, but are required to satisfy
          [Cohttp_lwt.S.Client]. *)
-    include (
-      C :
-        sig
-          type ctx = C.ctx
-
-          type 'a io = 'a C.io
-
-          type 'a with_context = 'a C.with_context
-
-          type body = C.body
-
-          val map_context : 'a with_context -> ('a -> 'b) -> 'b with_context
-
-          val set_cache : Cohttp_lwt.S.call -> unit
-        end)
+    include C
 
     let attrs_for ~uri ~meth:_ () =
       [
