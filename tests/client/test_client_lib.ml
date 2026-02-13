103886ec8305436baafe0e89c1a2942087d4c003
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-02-12 21:23:22 -0500

    remove obsolete test

--- tests/client/test_client_lib.ml
+++ tests/client/test_client_lib.ml
@@ -16,32 +16,7 @@ let test_config_printing () =
   in
   check' string ~msg:"is rendered correctly" ~actual ~expected
 
-let test_overriding_stateful_config () =
-  let module Env = Config.Env () in
-  Env.set_headers [ "foo", "bar" ];
-  Env.set_debug true;
-  let headers = [ "changed", "header" ] in
-  let debug = false in
-  let config : Config.t =
-    Env.make (fun common () -> common) ~debug ~headers ()
-  in
-  check'
-    (list (pair string string))
-    ~msg:"header is overriden" ~actual:(Env.get_headers ()) ~expected:headers;
-  check'
-    (list (pair string string))
-    ~msg:"config and stateful headers are the same" ~actual:(Env.get_headers ())
-    ~expected:config.headers;
-  check' bool ~msg:"debug is overriden" ~actual:(Env.get_debug ())
-    ~expected:debug;
-  check' bool ~msg:"config and stateful debug are the same"
-    ~actual:(Env.get_debug ()) ~expected:config.debug
-
 let suite =
-  [
-    test_case "default config pretty printing" `Quick test_config_printing;
-    test_case "overriding default stateful values via make constructor" `Quick
-      test_overriding_stateful_config;
-  ]
+  [ test_case "default config pretty printing" `Quick test_config_printing ]
 
 let () = Alcotest.run "Opentelemetry_client" [ "Config", suite ]
