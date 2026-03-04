f70e5ae4a2cee4368a3d5234002a7bb0900d5120
Simon Cruanes <simon.cruanes.2007@m4x.org>
2026-03-03 20:08:13 -0500

    fix test

--- tests/client/test_client_lib.ml
+++ tests/client/test_client_lib.ml
@@ -7,16 +7,15 @@ let test_config_printing () =
     Format.asprintf "%a" Config.pp @@ Env.make (fun common () -> common) ()
   in
   let expected =
-    "{ debug=false; log_level=none; sdk_disabled=false; self_trace=false;\n\
+    "{ debug=false; log_level=info; sdk_disabled=false; self_trace=false;\n\
     \ url_traces=\"http://localhost:4318/v1/traces\";\n\
     \ url_metrics=\"http://localhost:4318/v1/metrics\";\n\
     \ url_logs=\"http://localhost:4318/v1/logs\"; headers=[]; headers_traces=[];\n\
     \ headers_metrics=[]; headers_logs=[]; protocol=http/protobuf;\n\
     \ timeout_ms=10000; timeout_traces_ms=10000; timeout_metrics_ms=10000;\n\
-    \ timeout_logs_ms=10000; batch_traces=400; batch_metrics=200; \
-     batch_logs=400;\n\
-    \ batch_timeout_ms=2000; http_concurrency_level=None; retry_max_attempts=3;\n\
-    \ retry_initial_delay_ms=100; retry_max_delay_ms=5000;\n\
+    \ timeout_logs_ms=10000; traces={batch=400; timeout=2s}; metrics={batch=200;\n\
+    \ timeout=2s}; logs={batch=400; timeout=2s}; http_concurrency_level=None;\n\
+    \ retry_max_attempts=3; retry_initial_delay_ms=100; retry_max_delay_ms=5000;\n\
     \ retry_backoff_multiplier=2.0 }"
   in
   check' string ~msg:"is rendered correctly" ~actual ~expected
